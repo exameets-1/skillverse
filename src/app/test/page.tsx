8ee63d5881a7f6df6fb44a2c4b9cec04610c338b
santhosh-2504 <santhoshanantha58@gmail.com>
2025-10-18 10:00:44 +0530

    Fixed typeScript error

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -206,7 +206,7 @@ export default function TestPage() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: MouseEvent) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
     window.addEventListener('mousemove', handleMouseMove);
